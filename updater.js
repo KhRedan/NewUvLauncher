8f1c0af32e7b06b988e5ce9d56c8c5568810fca0
KhRedan <44591592+KhRedan@users.noreply.github.com>
2020-08-05 21:40:59 +0100

    first commit

---
+++ updater.js
@@ -0,0 +1,72 @@
+const os = require("os");
+const { Notification, app, dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+function appUpdater() {
+	autoUpdater.setFeedURL(
+		"https://github.com/KhRedan/NewUvLauncher/releases/download/v1.1.1/"
+	);
+	autoUpdater.checkForUpdates();
+	const checking = new Notification("update", {
+		body: "checking-for-update",
+	});
+
+	const available = new Notification("update", {
+		body: "update-available",
+	});
+
+	const unavailable = new Notification("update", {
+		body: "update-not-available",
+	});
+	autoUpdater.on("error", (err) => console.log(err));
+	autoUpdater.on("checking-for-update", () => {
+		checking.show();
+	});
+	autoUpdater.on("update-available", () => {
+		available.show();
+	});
+	autoUpdater.on("update-not-available", () => {
+		unavailable.show();
+	});
+
+	autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+		let message =
+			app.getName() +
+			" " +
+			releaseName +
+			" is now available. It will be installed the next time you restart the application.";
+		if (releaseNotes) {
+			const splitNotes = releaseNotes.split(/[^\r]\n/);
+			message += "\n\nRelease notes:\n";
+			splitNotes.forEach((notes) => {
+				message += notes + "\n\n";
+			});
+		}
+
+		dialog.showMessageBox(
+			{
+				type: "question",
+				buttons: ["Install", "Later"],
+				defaultId: 0,
+				message:
+					"A new version of " +
+					app.getName() +
+					" has been downloaded",
+				detail: message,
+			},
+			(response) => {
+				if (response === 0) {
+					setTimeout(() => autoUpdater.quitAndInstall(), 1);
+				}
+			}
+		);
+	});
+
+	setInterval(() => {
+		autoUpdater.checkForUpdates();
+	}, 3600000);
+}
+
+exports = module.exports = {
+	appUpdater,
+};
